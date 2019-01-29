c294b6ccb6cef7d94dec24df617a3982d4e95980
Tray Harris <traylee1513@yahoo.com>
2019-01-28 18:46:42 -0800

    Small change

--- Excercise1/Excercise1.cpp
+++ Excercise1/Excercise1.cpp
@@ -14,12 +14,12 @@ int main()
 	int ten = age + 10;
 	cout << "In 10 years, you will be " << ten << endl;
 
-	//Create the integer temp and prompt the user to input the current temperature, and write it into the variable "temp"
+	//Create the double temp and prompt the user to input the current temperature, and write it into the variable "temp"
 	double temp = 0.0;
 	cout << "Please enter the current temperature in Fahrenheit: ";
 	cin >> temp;
 
-	//Create the integer temp_c and use the expression to convert the inputed temperature to Celsius, then output the converted temperature.
+	//Create the double temp_c and use the expression to convert the inputed temperature to Celsius, then output the converted temperature.
 	double temp_c = (temp - 32) * 5 / 9;
 	cout << "The current temperature in Celsius is: " << temp_c;
 	return 0;
